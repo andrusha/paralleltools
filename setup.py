060d8a9c97b8a50add586f4a5e3fc556dce27ea1
Andrew Korzhuev <wolfon@gmail.com>
2012-11-05 08:15:46 +0400

    Moved tests to another directory, added readme to the distribution

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import os
 import sys
 from setuptools import setup
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
@@ -12,17 +13,17 @@ if sys.version_info <= (2, 5):
 
 setup(
     name="paralleltools",
-    version="0.0.2",
+    version="0.0.3",
     author="Andrew Korzhuev",
     author_email="korzhuev@andrusha.me",
     description=("A collection of basic list functions which can be run "
                  "in parallel mode (both sync or async)."),
-    long_description=read('README.md').split('=====')[2][:-6].strip(),
+    long_description=read('README').split('=====')[2][:-6].strip(),
     keywords="parallel threading map filter reduce async",
     license='MIT',
     url="http://github.com/andrusha/paralleltools",
-    packages=['paralleltools'],
-    test_suite='tests.runtests',
+    packages=['paralleltools', 'paralleltools.tests'],
+    test_suite='paralleltools.tests.runtests',
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
